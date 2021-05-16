a9064f75c524d96b8246573b912bfbc30d1df4f2
vinay-nb <vinaybhathg11@gmail.com>
2021-05-16 15:55:06 +0530

    admin ui design

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from re import I
 import sys
-from flask import Flask, request, render_template, jsonify, session, redirect
+from flask import Flask, request, render_template, jsonify, session, redirect, flash
 from flask.globals import session
 from functools import wraps
+from flask.helpers import flash, url_for
 # import pymongo
 # from env.users.models import User
 from flask_pymongo import PyMongo
 from passlib.hash import sha256_crypt
 import uuid
+import hashlib
 
 app = Flask(__name__)
 app.secret_key = b'\rv\x88\xeb\x97$2\x96\x1f\x02Z)\xc6\x06\xde\xc9'
@@ -48,12 +50,21 @@ def admin():
 
 @app.route('/admin/login/', methods=['POST', 'GET'])
 def adminLogin():
-    print('inlogin function')
-    user = db.admin1.find_one({
-        "email": request.form.get('email')
-    })
-    if user and request.form.get('password') == user['password']:
+    if request.method == 'GET':
         return render_template("alogin.html")
+    else:
+        print('inlogin function')
+        user = db.admin1.find_one({
+            "email": request.form.get('email')
+        })
+        user_password = request.form.get('password')
+        if user_password:
+            user_password = hashlib.md5(user_password.encode('utf-8')).hexdigest()
+        if user and user_password == user['password']:
+            print('login successful')
+            return render_template("dashboard.html")
+        else:
+            flash('Email Or Password Is Incorrect')
         # return startSession(user)
     # return jsonify({"error": "account doesnot exist"}), 401
 @app.route('/admin/signout')
@@ -62,7 +73,7 @@ def signout():
 
 class User:
     def startSession(self, user):
-        del user['passowrd']
+        del user['password']
         print('session')
         session['logged_in'] = True
         session['user'] = user
@@ -72,6 +83,7 @@ class User:
         # print(request.form)
         print("in signup function")
         # create user object
+        
         admin = {
             "_id": uuid.uuid4().hex,
             "name": request.form.get('name'),
@@ -80,19 +92,24 @@ class User:
         }
         # print(jsonify(admin))
         #encrypt password
-        # hash_password = sha256_crypt.encrypt(admin['password'])
-        # admin['password'] = hash_password
+        # if admin['email']:
+        #     flash('email already exists')
+        password = admin['password']
+        if password:
+            hash_password = hashlib.md5(admin['password'].encode('utf-8'))
+            admin['password'] = hash_password.hexdigest()
         # print(jsonify(admin))
 
         #check email exists
         if(db.admin1.find_one({"email" : admin['email']})):
-            return jsonify({"error": "email exits"}), 400
+            flash('Email Id Already Exists')
         print("saving")
         
         if (db.admin1.save(admin)):
+            flash('Registered Successfully')
             return self.startSession(admin)       
         
-        return jsonify({"error": "Signup failed"})
+        return flash('Sign Up Failed!!')
 
     def signout(self):
         session.clear()
@@ -102,3 +119,5 @@ if __name__ == "__main__":
     # app.init_db()
     app.run(debug=True)
     # app = create_app()
+
+
