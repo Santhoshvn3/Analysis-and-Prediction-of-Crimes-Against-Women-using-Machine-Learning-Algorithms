9e1974f6d7fcefc8b0f03d0d7bcd70c0f6c054d4
vinay-nb <vinaybhathg11@gmail.com>
2021-06-01 09:12:06 +0530

    password reset working

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from re import I
+import re
 import sys, os
 from flask import Flask, request, render_template, jsonify, session, redirect, flash
 from flask.globals import session
@@ -18,6 +19,7 @@ import numpy as np
 from .local_settings import password
 
 app = Flask(__name__)
+# app = Flask(static_folder='/home/vinay/APCW/env/static')
 app.secret_key = b'\rv\x88\xeb\x97$2\x96\x1f\x02Z)\xc6\x06\xde\xc9'
 
 # mail config
@@ -29,7 +31,7 @@ app.config['MAIL_PASSWORD'] = password()
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
-otp = randint(000000, 999999) 
+otp = int(randint(000000, 999999))
 
 # database
 mongodb_client = PyMongo(app, uri="mongodb://localhost:27017/adminUser")
@@ -62,6 +64,9 @@ def analysis():
 def dashboard():
     return render_template("dashboard.html")
 
+@app.route('/about/')
+def about():
+    return render_template('about.html')
 
 @app.route('/admin/signup/', methods=['GET', 'POST'])
 def admin():
@@ -93,9 +98,44 @@ def adminLogin():
 def signout():
     return User().signout()
 
+
 @app.route('/admin/forgotpassword/', methods=['GET', 'POST'])
 def forgotPassword():
+    if request.method == "POST":
+        url = request.host_url + 'admin/resetPassword/'
+        email1 = request.form.get('email')
+        if not email1:
+            flash('please enter email')
+            return (request.url)
+        # email1 = str(email1)
+        email_found = db.admin1.find({"email": email1}).count() > 0
+        print("email found is",email_found)
+        if email_found > 0:
+            msg = Message(subject="reset password from analysis and prediction of crime against women", sender="vinaybhathg11@fmail.com", recipients=[email1])
+            msg.body = url
+            mail.send(msg)
+            flash("email sent successfully", "success")
+        else:
+            flash("email not exists", "warning")
+            return redirect(request.url)
     return render_template('forgot_password.html')
+
+@app.route('/admin/resetPassword/', methods=['GET', 'POST'])
+def resetPassword():
+    if request.method == 'POST':
+        email1 = request.form.get('email')
+        user_password = request.form.get('cpassword')
+        if user_password:
+            hash_password = hashlib.md5(user_password.encode('utf-8'))
+            user_password = hash_password.hexdigest()
+        # user_email =  db.admin1.find({"email": email1})
+        print("email found reset", email1)
+        flag = db.admin1.find_one_and_update({"email": email1}, { "$set": {"password": user_password}})
+        print('updation', flag)
+        if (flag):
+            flash("password reset sucessfull login with new password", "success")
+    return render_template('reset_password.html')
+
 
 @app.route('/admin/verify/', methods=['GET', 'POST'])
 def verifyEmail(email):
@@ -105,15 +145,37 @@ def verifyEmail(email):
     mail.send(msg)
     return render_template('verify.html')
 
+@app.route('/admin/verfiylogin/', methods=['GET', 'POST'])
+def verfiyAdmin():
+    return render_template("dashboard.html")
+    # else:
+    #     print('inlogin function')
+    #     user = db.admin1.find_one({
+    #         "email": request.form.get('email')
+    #     })
+    #     user_password = request.form.get('password')
+    #     if user_password:
+    #         user_password = hashlib.md5(user_password.encode('utf-8')).hexdigest()
+    #     if user and user_password == user['password']:
+    #         # flash("login successful", "success")
+    #         return render_template("dashboard.html")
+    #     else:
+    #         flash("Email or Password Is Incorrect", "warning")
+    #         return redirect(request.url)
+    # return render_template('alogin.html')
+
 @app.route('/admin/validate/', methods=['GET', 'POST'])
 def validate():
-    user_otp = request.form.get('pin')
-    if otp == int(user_otp):
-        flash('Email verification successfull', "success")
-        return render_template('alogin.html')
-    else:
-        flash('Email verification failed', "warning")
-        return render_template('verify.html')
+    if request.method == 'POST':
+        print('inside if')
+        user_otp = request.form.get('pin')
+        if otp == int(user_otp):
+            flash('Email verification successfull', "success")
+            # if request.method == 'GET':
+            return verfiyAdmin()
+        else:
+            flash('Email verification failed', "warning")
+            return render_template('verify.html')
     
 
 class User:
@@ -169,73 +231,79 @@ def pred():
 
 @app.route('/women', methods=['GET','POST'])
 def women():
-    year = request.form.get("Predict_Year")  # Year fetching From UI.
-    C_type = request.form.get("C_Type")  # Crime type fetching from UI
-    state = request.form.get("state")  # State name fetching from UI
+    year = request.form.get("Predict_Year")	#Year fetching From UI.
+    C_type = request.form.get("C_Type")	#Crime type fetching from UI
+    state = request.form.get("state")	#State name fetching from UI
+    
+    df = pd.read_csv("static/StateWiseCAWPred1990-2019.csv", header=None)
 
-    df = pd.read_csv("static/StateWiseCAWPred1990-2016.csv", header=None)
-
-    data1 = df.loc[df[0] == state].values  # Selecting State and its attributes.
+    data1 = df.loc[df[0]==state].values			#Selecting State and its attributes.
     for x in data1:
-        if x[1] == C_type:
-            test = x
-            break
-
+	    if x[1] == C_type:
+		    test = x
+		    break
+    
     l = len(df.columns)
     trendChangingYear = 2
 
-    xTrain = np.array(
-        [2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018,
-         2019])
+    xTrain = np.array([2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019])
     yTrain = test[2:29]
 
-    X = df.iloc[0, 2:l].values
+    X = df.iloc[0,2:l].values
     y = test[2:]
-    regressor = LinearRegression()  # regression algorithm cealled.
-    regressor.fit(X.reshape(-1, 1), y)  # Data set is fitted in regression and Reshaped it.
-    accuracy = regressor.score(X.reshape(-1, 1), y)  # Finding Accuracy of Predictions.
-    print(accuracy)
+    regressor = LinearRegression()		#regression algorithm cealled.
+    regressor.fit(X.reshape(-1,1),y)	#Data set is fitted in regression and Reshaped it.
+    accuracy = regressor.score(X.reshape(-1,1),y)	#Finding Accuracy of Predictions.
+    print (accuracy)
     accuracy_max = 0.10;
 
-    # Trending year(Influence Year) finding algorithm.
-    if (accuracy < 0.10):  # Used 65% accuracy as benchmark for trending year finding algorithm.
-        for a in range(3, l - 8):
-
-            X = df.iloc[0, a:l].values
+	#Trending year(Influence Year) finding algorithm.
+    if(accuracy < 0.50):			#Used 65% accuracy as benchmark for trending year finding algorithm.
+        for a in range(3,l-8):
+            X = df.iloc[0,a:l].values
             y = test[a:]
             regressor = LinearRegression()
-            regressor.fit(X.reshape(-1, 1), y)
-            accuracy = regressor.score(X.reshape(-1, 1), y)
+            regressor.fit(X.reshape(-1,1),y)
+            accuracy = regressor.score(X.reshape(-1,1),y)
             if (accuracy > accuracy_max):
                 accuracy_max = accuracy
-                print(accuracy_max)
+                print (accuracy_max)
                 trendChangingYear = a
-    print(trendChangingYear)  # Printing Trend Changing Year on server terminal.
-    print(test[trendChangingYear])
-    print(xTrain[trendChangingYear - 2])
+    print (trendChangingYear)			#Printing Trend Changing Year on server terminal.
+    print (test[trendChangingYear])
+    print (xTrain[trendChangingYear-2])
     year = int(year)
     y = test[2:]
     b = []
 
-    for j in range(2023, year + 1):
-        prediction = regressor.predict(np.array([[j]]))
-        if (prediction < 0):
-            prediction = 0
-        y = np.append(y, prediction)
-    y = np.append(y, 0)
+	#If accuracy is Lower than 65%, only visualization of the data is shown - no predictions
+    if accuracy < 0.50:
+        for k in range(2001,2019):
+            a = str(k)
+            b = np.append(b,a)
+        y = list(y)
+        yearLable = list(b)
+        msg = "Data is not Sutaible for prediction"
 
-    for k in range(2001, year + 1):
-        a = str(k)
-        b = np.append(b, a)
-    y = list(y)
-    yearLable = list(b)
-    msg = ""
+	#Else predictions are shown and Run time data and labels are added to the graph.
+    else:
+        for j in range(2021,year+1):
+            prediction = regressor.predict(np.array([[j]]))
+            if(prediction < 0):
+                prediction = 0
+            y = np.append(y,prediction)
+        y = np.append(y,0)
+        
+        for k in range(2001,year+1):
+            a = str(k)
+            b = np.append(b,a)
+        y = list(y)
+        yearLable = list(b)
+        msg = ""
     if C_type == "ASSAULT ON WOMEN WITH INTENT TO OUTRAGE HER MODESTY":
         C_type = "ASSAULT ON WOMEN"
-    # Finally the template is rendered
-    return render_template('women.html', data=[accuracy, yTrain, xTrain, state, year, data1, X, y, test, l], msg=msg,
-                           state=state, year=year, C_type=C_type, pred_data=y, years=yearLable)
-
+	#Finally the template is rendered
+    return render_template('women.html',data = [accuracy,yTrain,xTrain,state,year,data1,X,y,test,l],msg = msg,state=state, year=year, C_type=C_type,pred_data = y,years = yearLable)
 
 if __name__ == "__main__":
     # app.init_db()
